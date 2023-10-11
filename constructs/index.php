7984150d9414c3794326e91858ca0771dd973409
victor braz <victor.90braz@gmail.com>
2023-10-11 17:42:30 +0200

    add Subscription

--- constructs/index.php
+++ constructs/index.php
@@ -2,22 +2,23 @@
 $basePath = "C:\\Users\\braz9\\Desktop\\projects\\laracasts\\object-oriented-principles-php\\07\\";
 require_once $basePath . "functions.php";
 
-class Person {
+// Object Composition and Abstractions
 
-    public $name;
+class Subscription {
 
-    public function __construct($name) {
-        $this->$name;
+    public function create() {
+
     }
 
-    private function password() {
-        return 'hello world';
+    public function cancel() {
+
+    }
+
+    public function invoice() {
+
+    }
+
+    public function swap($newPlan) {
+
     }
 }
-
-$method = new \ReflectionMethod(Person::class, 'password');
-$method->setAccessible(true);
-
-$person = new Person('Bob');
-
-dd($method->invoke($person));
